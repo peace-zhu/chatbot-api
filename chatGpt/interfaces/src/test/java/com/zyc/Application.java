2e734198db969611221f245964a42c1c19615eea
peace-zhu <2498430685@qq.com>
2023-03-22 16:57:37 +0800

    初始化ddd工程

---
+++ chatGpt/interfaces/src/test/java/com/zyc/Application.java
@@ -0,0 +1,10 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api.class,args);
+    }
+}
